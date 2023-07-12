965ed2fbd2acc3a08bf6430524e6a145822433fa
Rodrigo Ferreira <rodrigo_ferreira89@outlook.com>
2023-07-11 23:05:57 -0300

    subindo de casa

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 const btnMenu = document.querySelector('.header-buttonMenu');
-
-const imgs = document.getElementById('img');
-const img = document.querySelectorAll("#img img");
-
+const imgsModal = document.querySelectorAll('.img-modal');
+const btnModalFechar = document.querySelector('.btnDialog')
+const imgArray = [
+    "img/banner-mobile-1.webp",
+    "img/banner-mobile-2.webp",
+    "img/banner-mobile-3.webp",
+    "img/banner-mobile-4.webp",
+    "img/banner-mobile-5.webp"
+]
+const faqClick = document.querySelectorAll('.faq-h3')
 
 const ativarMenuResponsivo = () => {
     const nav = document.querySelector('.header-navegacao')
@@ -32,26 +38,38 @@ const verificarLarguraTela = () => {
         const nav = document.querySelector('.header-navegacao')
         nav.classList.remove('ativar');
     }
+}
+
+const exibirModal = (e) => {
+    let att = e.target.getAttribute('data-img')
+    const dialog = document.querySelector('.projetos-dialog')
+    const imgDialog = document.querySelector('.exibir-dialog')
+    imgDialog.src = imgArray[att]
+    dialog.style.display = 'flex'
 
 }
 
-let idx = 0;
-
-function carroussel() {
-    idx++
-
-    if(idx > img.length - 4) {
-        
-    }
-
-    imgs.style.transform = `translateX(${-idx *300}px)`;
-}
-
+const exibirFaq = (e) => {
+    const numeroAtributo = e.target.getAttribute('data-faq')
+    console.log(numeroAtributo)
+    const faqP = document.querySelectorAll('.faq-p');
     
+    faqP[numeroAtributo].classList.toggle('ativar-faq-p')
+}
 
-setInterval(carroussel, 3000);
-
+const fecharDialog = () => {
+    document.querySelector('.projetos-dialog').style.display = 'none'
+}
 
 btnMenu.addEventListener('click', ativarMenuResponsivo);
 window.addEventListener('scroll', mudarCorHeader)
 window.addEventListener('resize', verificarLarguraTela);
+imgsModal.forEach((elemento) => {
+    elemento.addEventListener('click', exibirModal)
+})
+btnModalFechar.addEventListener('click', fecharDialog)
+faqClick.forEach((elemento) => {
+    elemento.addEventListener('click', exibirFaq)
+} )
+
+
